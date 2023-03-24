c25ee24da4b695ce15b77a465ef2b362b21c7c69
MAR-IT <125657825+MAR-IT@users.noreply.github.com>
2023-03-24 11:51:22 +0100

    document ready eryuit

--- script12-2.js
+++ script12-2.js
@@ -1,10 +1,8 @@
-$(document).ready(function() {
-  $.ajax({
-    url: 'https://mar-it.github.io/JavaLes12/content12.html',
-    success: function(data) {
-      $('#divResult').html(data);
-    },
-    error: function(){
-      $('#divResult').append('Fout: er is iets fout gegaan')
-    }
+$.ajax({
+  url: 'https://mar-it.github.io/JavaLes12/content12.html',
+  success: function(data) {
+    $('#divResult').append(data);
+  },
+  error: function(){
+    $('#divResult').append('Fout: er is iets fout gegaan')
   });
