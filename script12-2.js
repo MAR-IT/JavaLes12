b534d4e4f14f5a0175122f658ba0d2bc4a4e0b64
MAR-IT <125657825+MAR-IT@users.noreply.github.com>
2023-03-24 11:16:32 +0100

    kommas

--- script12-2.js
+++ script12-2.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
   $.ajax({
-    url: 'https://mar-it.github.io/JavaLes12/content12.html'
+    url: 'https://mar-it.github.io/JavaLes12/content12.html',
     success: function(data) {
       $('#divResult').html(data);
-    };
+    },
     error: function(){
       $('#divResult').append('Fout: er is iets fout gegaan');
     };
