7be9e26235dd58c0d553cf1eb2fd7688dd84758f
MAR-IT <125657825+MAR-IT@users.noreply.github.com>
2023-03-24 09:05:22 +0100

    pagina laden

--- script12-1.js
+++ script12-1.js
@@ -1,1 +1,3 @@
-$('#divResult').load(https://mar-it.github.io/JavaLes12/content12.html)
+$(document).ready(function laden() {
+  $('#divResult').load(https://mar-it.github.io/JavaLes12/content12.html)
+});
