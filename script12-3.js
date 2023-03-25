1125e303f8c6cddd9e06f5a99f4804f75f803a5a
MAR-IT <125657825+MAR-IT@users.noreply.github.com>
2023-03-25 07:58:57 +0100

    naam gemeente

--- script12-3.js
+++ script12-3.js
@@ -2,8 +2,11 @@
 
 $.ajax({
   url: 'https://api.openweathermap.org/data/2.5/weather?appid=8566d604cd9402b65394b034e52aa2af&q=Nijkerk',
+  dataType: 'jsonp',
+  contentType: 'application/jsonp',
   success: function(weather) {
     console.log(weather);
+    $('h1').append(weather.name);
   },
   error: function(){
     $('#divResult').append('Fout: er is iets fout gegaan')
