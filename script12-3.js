f0c9feb4c283700011a1297aea8af7b20a4b3622
MAR-IT <125657825+MAR-IT@users.noreply.github.com>
2023-03-25 08:44:27 +0100

    omrekenen weg

--- script12-3.js
+++ script12-3.js
@@ -7,7 +7,7 @@ $.ajax({
   success: function(weather) {
     console.log(weather);
     $('h1').append(weather.name);
-    $('#temp').append(((weather.main.temp_min - 32)/1.8) + ' en ' + weather.main.temp_max + ' graden.');
+    $('#temp').append(weather.main.temp_min + ' en ' + weather.main.temp_max + ' graden.');
     $('#gevoel').append(weather.main.feels_like  + ' graden');
     $('#vocht').append(weather.main.humidity + '%');
   },
