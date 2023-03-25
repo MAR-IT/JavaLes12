0ac8efde5c2f3c6fb28168391eacf856bf32b4f9
MAR-IT <125657825+MAR-IT@users.noreply.github.com>
2023-03-25 08:21:59 +0100

    nl weg

--- script12-3.js
+++ script12-3.js
@@ -1,13 +1,13 @@
 
 
 $.ajax({
-  url: 'https://api.openweathermap.org/data/2.5/weather?appid=8566d604cd9402b65394b034e52aa2af&q=Nijkerk&units=metric&lang=nl',
+  url: 'https://api.openweathermap.org/data/2.5/weather?appid=8566d604cd9402b65394b034e52aa2af&q=Nijkerk&units=metric',
   dataType: 'jsonp',
   contentType: 'application/jsonp',
   success: function(weather) {
     console.log(weather);
     $('h1').append(weather.name);
-    $('#temp').append(weather.main.temp_min + ' en ' + weather.temp_max + ' graden.');
+    $('#temp').append(weather.main.temp_min + ' en ' + weather.main.temp_max + ' graden.');
     $('#gevoel').append(weather.main.feels_like  + ' graden');
     $('#vocht').append(weather.main.humidity + '%');
   },
